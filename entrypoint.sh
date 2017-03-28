0ea50d75bc9e67c6fdc966d22564d46f2eaf2edd
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-03-28 18:35:26 +0200

    Add GID and UID options, change default umask to 022

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,20 @@ mount --make-shared /home/shared/s3
 
 mkdir -p /home/shared/s3
 mkdir -p /tmp
-s3fs $BUCKETNAME /home/shared/s3 -o use_cache=/tmp -o allow_other -o umask=0002 -o use_rrs -f &
+
+if test -n $GID; then
+  groupadd -g $GID s3fs
+  GID_OPT="-o gid=${GID}"
+fi
+
+if test -n $UID; then
+  useradd -u $UID s3fs
+  UID_OPT="-o uid=${UID}"
+fi
+
+s3fs $BUCKETNAME /home/shared/s3 -o use_cache=/tmp \
+  $GID_OPT $UID_OPT \
+  -o allow_other -o umask=022 -o use_rrs -f &
 
 wait
 sleep 2
