606057898a30363e460f7e8708ea54f285a18f5e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-03-29 14:56:28 +0200

    Better option mgmt

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,27 @@ mount --make-shared /home/shared/s3
 mkdir -p /home/shared/s3
 mkdir -p /tmp
 
-if test -n $GID; then
-  groupadd -g $GID s3fs
-  GID_OPT="-o gid=${GID}"
+OPTS="-o use_cache=/tmp -o allow_other -o umask=022 -o use_rrs -f"
+
+if ! test -z "$S3FS_GID"; then
+  groupadd -g $S3FS_GID s3fs
+  OPTS="${OPTS} -o gid=${S3FS_GID}"
 fi
 
-if test -n $UID; then
-  useradd -u $UID s3fs
-  UID_OPT="-o uid=${UID}"
+if ! test -z "$S3FS_UID"; then
+  useradd -u $S3FS_UID s3fs
+  OPTS="${OPTS} -o uid=${S3FS_UID}"
 fi
 
-s3fs $BUCKETNAME /home/shared/s3 -o use_cache=/tmp \
-  $GID_OPT $UID_OPT \
-  -o allow_other -o umask=022 -o use_rrs -f &
+if ! test -z "$S3FS_URL"; then
+  OPTS="${OPTS} -o url=${S3FS_URL}"
+fi
+
+if ! test -z "$DEBUG_LEVEL"; then
+  OPTS="${OPTS} -o dbglevel=${DEBUG_LEVEL}"
+fi
+
+s3fs $BUCKETNAME /home/shared/s3 $OPTS $EXTRA_OPTS &
 
 wait
 sleep 2
